2615133ba5226083d4f92d3d7d2c30b0c624f44c
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-08 20:03:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var artistInstrumentArray = {}
+  var i
   for (i = 0; i < musicians.length - 1; i++) {
     artistInstrument = musicians[i] + " plays" + instruments[i]
     artistInstrumentArray.push(artistInstrument)
